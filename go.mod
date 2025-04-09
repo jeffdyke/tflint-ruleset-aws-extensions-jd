5acc04b7b9afe83ff0de52e6589d1df29c8e8bf0
Jeff Dyke <jeff.dyke@gmail.com>
2025-04-09 01:36:47 -0400

    work

--- go.mod
+++ go.mod
@@ -1,10 +1,13 @@
 module github.com/terraform-linters/tflint-ruleset-template
 
-go 1.24.0
+go 1.24.1
+
+toolchain go1.24.2
 
 require (
 	github.com/hashicorp/hcl/v2 v2.23.0
 	github.com/terraform-linters/tflint-plugin-sdk v0.22.0
+	github.com/terraform-linters/tflint-ruleset-terraform v0.12.0
 )
 
 require (
@@ -12,7 +15,7 @@ require (
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/hashicorp/go-hclog v1.6.3 // indirect
 	github.com/hashicorp/go-plugin v1.6.2 // indirect
 	github.com/hashicorp/go-version v1.7.0 // indirect
@@ -23,7 +26,7 @@ require (
 	github.com/oklog/run v1.0.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	github.com/zclconf/go-cty v1.16.0 // indirect
+	github.com/zclconf/go-cty v1.16.2 // indirect
 	golang.org/x/mod v0.22.0 // indirect
 	golang.org/x/net v0.36.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
--- rules/aws_security_group_egress.go
+++ rules/aws_security_group_egress.go
@@ -4,67 +4,77 @@ import (
 	"fmt"
 
 	"github.com/terraform-linters/tflint-plugin-sdk/hclext"
-	"github.com/terraform-linters/tflint-plugin-sdk/logger"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
+	"github.com/terraform-linters/tflint-ruleset-terraform/project"
 )
 
-// AwsInstanceEgressRule checks whether ...
-type AwsInstanceEgressRule struct {
+// AwsSecurityGroupEgressRule checks whether ...
+type AwsSecurityGroupEgressRule struct {
 	tflint.DefaultRule
+	resourceType    string
+	subResourceType string
+	attributeName   string
 }
 
-// NewAwsInstanceEgressRule returns a new rule
-func NewAwsInstanceEgressRule() *AwsInstanceEgressRule {
-	return &AwsInstanceEgressRule{}
+// NewAwsSecurityGroupEgressRule returns a new rule
+func NewAwsSecurityGroupEgressTypeRule() *AwsSecurityGroupEgressRule {
+	return &AwsSecurityGroupEgressRule{
+		resourceType:    "aws_security_group",
+		subResourceType: "egress",
+		attributeName:   "cidr_blocks",
+	}
 }
 
 // Name returns the rule name
-func (r *AwsInstanceEgressRule) Name() string {
-	return "aws_extension_egress_no_shared_sg"
+func (r *AwsSecurityGroupEgressRule) Name() string {
+	return "aws_security_group_egress"
 }
 
 // Enabled returns whether the rule is enabled by default
-func (r *AwsInstanceEgressRule) Enabled() bool {
+func (r *AwsSecurityGroupEgressRule) Enabled() bool {
 	return true
 }
 
 // Severity returns the rule severity
-func (r *AwsInstanceEgressRule) Severity() tflint.Severity {
+func (r *AwsSecurityGroupEgressRule) Severity() tflint.Severity {
 	return tflint.ERROR
 }
 
 // Link returns the rule reference link
-func (r *AwsInstanceEgressRule) Link() string {
-	return ""
+func (r *AwsSecurityGroupEgressRule) Link() string {
+	return project.ReferenceLink(r.Name())
 }
 
 // Check checks whether ...
-func (r *AwsInstanceEgressRule) Check(runner tflint.Runner) error {
+func (r *AwsSecurityGroupEgressRule) Check(runner tflint.Runner) error {
 	// This rule is an example to get a top-level resource attribute.
-	resources, err := runner.GetResourceContent("aws_security_group", &hclext.BodySchema{
+	resources, err := runner.GetResourceContent(r.resourceType, &hclext.BodySchema{
+		Blocks: []hclext.BlockSchema{
+			{Type: r.subResourceType},
+		},
 		Attributes: []hclext.AttributeSchema{
-			{Name: "egress"},
+			{Name: r.attributeName},
 		},
 	}, nil)
 	if err != nil {
 		return err
 	}
 
-	// Put a log that can be output with `TFLINT_LOG=debug`
-	logger.Debug(fmt.Sprintf("Get %d egress rules", len(resources.Blocks)))
-
 	for _, resource := range resources.Blocks {
-		attribute, exists := resource.Body.Attributes["cidr_blocks"]
+		attribute, exists := resource.Body.Attributes[r.attributeName]
 		if !exists {
 			continue
 		}
 
-		err := runner.EvaluateExpr(attribute.Expr, func(egressCidrBlock string) error {
-			return runner.EmitIssue(
-				r,
-				fmt.Sprintf("egress.cidr_blocks is %s", egressCidrBlock),
-				attribute.Expr.Range(),
-			)
+		err := runner.EvaluateExpr(attribute.Expr, func(cidr string) error {
+			if cidr != "[0.0.0.0/0]" {
+				runner.EmitIssue(
+					r,
+					fmt.Sprintf("\"%s\" is an invalid cidr block.", cidr),
+					attribute.Expr.Range(),
+				)
+			}
+			return nil
 		}, nil)
 		if err != nil {
 			return err
@@ -72,4 +82,46 @@ func (r *AwsInstanceEgressRule) Check(runner tflint.Runner) error {
 	}
 
 	return nil
+
+	// resources, err := runner.GetResourceContent(r.resourceType, &hclext.BodySchema{
+	// 	Blocks: []hclext.BlockSchema{
+	// 		{
+	// 			Type: r.subResourceType,
+	// 			Body: &hclext.BodySchema{
+	// 				Attributes: []hclext.AttributeSchema{
+	// 					{Name: r.attributeName},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }, nil)
+
+	// if err != nil {
+	// 	return err
+	// }
+
+	// for _, resource := range resources.Blocks {
+	// 	for _, rule := range resource.Body.Blocks {
+	// 		attribute, exists := rule.Body.Attributes["egress"]
+	// 		if !exists {
+	// 			continue
+	// 		}
+
+	// 		err := runner.EvaluateExpr(attribute.Expr, func(cidr string) error {
+	// 			if cidr != "0.0.0.0/0" {
+	// 				runner.EmitIssue(
+	// 					r,
+	// 					fmt.Sprintf("\"%s\" is an invalid cidr block.", cidr),
+	// 					attribute.Expr.Range(),
+	// 				)
+	// 			}
+	// 			return nil
+	// 		}, nil)
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	// 	}
+	// }
+
+	// return nil
 }
